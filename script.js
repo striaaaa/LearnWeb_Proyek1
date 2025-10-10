cfc935f31c812757c3e04ef0aa5f0c4eb33c3cd0
striaaaa <satriasahrul2@gmail.com>
2025-10-10 08:11:31 +0700

    update js

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ const toggleButton = document.getElementById("toggleButton");
 const body = document.body;
 const icon = document.getElementById("icon");
 
-// Event listener for the toggle button
 toggleButton.addEventListener("click", () => {
   body.classList.toggle("dark");
   if (body.classList.contains("dark")) {
-    icon.className = "ri-moon-line"; // Change to moon icon
+    icon.className = "ri-moon-line";
   } else {
-    icon.className = "ri-sun-fill"; // Change to sun icon using Font Awesome
+    icon.className = "ri-sun-fill";
   }
 });
